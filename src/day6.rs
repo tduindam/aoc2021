9bda95c361f3f2abab53f4f4e4112951ec6c6ca0
Thomas <tduindam@gmail.com>
2021-12-07 12:16:47 +0100

    Day 7

--- src/day6.rs
+++ src/day6.rs
@@ -1,21 +1,16 @@
+use crate::reader::parse_ints;
+
 pub fn main() {
     let input = "5,1,1,3,1,1,5,1,2,1,5,2,5,1,1,1,4,1,1,5,1,1,4,1,1,1,3,5,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,4,1,1,1,1,1,5,1,1,1,4,1,1,1,1,1,3,1,1,4,1,4,1,1,2,3,1,1,1,1,4,1,2,2,1,1,1,1,1,1,3,1,1,1,1,1,2,1,1,1,1,1,1,1,4,4,1,4,2,1,1,1,1,1,4,3,1,1,1,1,2,1,1,1,2,1,1,3,1,1,1,2,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,2,1,1,2,3,1,2,1,1,4,1,1,5,3,1,1,1,2,4,1,1,2,4,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,4,3,1,2,1,2,1,5,1,2,1,1,5,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,1,5,1,1,1,1,5,1,4,1,1,1,4,1,3,4,1,4,1,1,1,1,1,1,1,1,1,3,5,1,3,1,1,1,1,4,1,5,3,1,1,1,1,1,5,1,1,1,2,2";
-    let input = parse_input(input);
-    println!("Day 6-1 {}", run(input, 80).unwrap());
+    let input = parse_ints(input, ",");
+    println!("Day 6-1 {}", run(input, 80));
 
     let input = "5,1,1,3,1,1,5,1,2,1,5,2,5,1,1,1,4,1,1,5,1,1,4,1,1,1,3,5,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,4,1,1,1,1,1,5,1,1,1,4,1,1,1,1,1,3,1,1,4,1,4,1,1,2,3,1,1,1,1,4,1,2,2,1,1,1,1,1,1,3,1,1,1,1,1,2,1,1,1,1,1,1,1,4,4,1,4,2,1,1,1,1,1,4,3,1,1,1,1,2,1,1,1,2,1,1,3,1,1,1,2,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,2,1,1,2,3,1,2,1,1,4,1,1,5,3,1,1,1,2,4,1,1,2,4,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,4,3,1,2,1,2,1,5,1,2,1,1,5,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,1,5,1,1,1,1,5,1,4,1,1,1,4,1,3,4,1,4,1,1,1,1,1,1,1,1,1,3,5,1,3,1,1,1,1,4,1,5,3,1,1,1,1,1,5,1,1,1,2,2";
-    let input = parse_input(input);
-    println!("Day 6-2 {}", run(input, 256).unwrap());
+    let input = parse_ints(input, ",");
+    println!("Day 6-2 {}", run(input, 256));
 }
 
-fn parse_input(input: &str) -> Vec<u32> {
-    input
-        .split(",")
-        .filter_map(|v| v.parse::<u32>().ok())
-        .collect()
-}
-
-fn run(fishes: Vec<u32>, days: u32) -> Result<u64, Day6Err> {
+fn run(fishes: Vec<u32>, days: u32) -> u64 {
     let mut histogram = [0u64; 9];
     for f in fishes {
         histogram[f as usize] += 1u64;
@@ -37,7 +32,7 @@ fn run(fishes: Vec<u32>, days: u32) -> Result<u64, Day6Err> {
         t += 1;
     }
 
-    Ok(histogram.iter().sum())
+    histogram.iter().sum()
 }
 
 #[cfg(test)]
@@ -47,23 +42,14 @@ mod tests {
     #[test]
     fn part_one() {
         let input = "3,4,3,1,2";
-        let input = parse_input(input);
+        let input = parse_ints(input, ",");
 
-        assert_eq!(26, run(input, 18).unwrap());
-        // assert_eq!(26, run_bruteforce(input, 18).unwrap());
+        assert_eq!(26, run(input, 18));
 
-        let input = "3,4,3,1,2";
-        let input: Vec<u32> = input
-            .split(",")
-            .filter_map(|v| v.parse::<u32>().ok())
-            .collect();
-        assert_eq!(5934, run(input, 80).unwrap());
+        let input = vec![3, 4, 3, 1, 2];
+        assert_eq!(5934, run(input, 80));
 
-        let input = "3,4,3,1,2";
-        let input: Vec<u32> = input
-            .split(",")
-            .filter_map(|v| v.parse::<u32>().ok())
-            .collect();
-        assert_eq!(26984457539, run(input, 256).unwrap());
+        let input = vec![3, 4, 3, 1, 2];
+        assert_eq!(26984457539, run(input, 256));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 mod reader;
 
 fn main() {
@@ -28,6 +29,7 @@ fn main() {
             4 => day4::main(),
             5 => day5::main(),
             6 => day6::main(),
+            7 => day7::main(),
             _ => eprintln!("No such day ({})", day),
         },
         Err(e) => eprintln!("Could not parse input {:?}", e),
