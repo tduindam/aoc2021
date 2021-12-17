ac7c248d4689b378d7022ed379ab66a16f5d3d79
Thomas <tduindam@gmail.com>
2021-12-17 20:54:22 +0100

    day 16-2

--- src/day16.rs
+++ src/day16.rs
@@ -31,10 +31,38 @@ lazy_static! {
     ]);
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Type {
+    Sum,
+    Product,
+    Minimum,
+    Maximum,
+    Literal,
+    Greater,
+    Less,
+    Equal,
+}
+
+impl Type {
+    fn from_u64(id: u64) -> Self {
+        match id {
+            0 => Self::Sum,
+            1 => Self::Product,
+            2 => Self::Minimum,
+            3 => Self::Maximum,
+            4 => Self::Literal,
+            5 => Self::Greater,
+            6 => Self::Less,
+            7 => Self::Equal,
+            _ => unreachable!(),
+        }
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 struct Packet {
     version: u64,
-    type_id: u64,
+    type_id: Type,
     payload: PacketPayload,
 }
 
@@ -93,9 +121,9 @@ fn parse_operator(input: &str) -> IResult<&str, PacketPayload> {
     alt((parse_fixed_length, parse_number_packets))(input)
 }
 
-fn payload(input: &str, type_id: u64) -> IResult<&str, PacketPayload> {
+fn payload(input: &str, type_id: Type) -> IResult<&str, PacketPayload> {
     match type_id {
-        4 => parse_literal(input),
+        Type::Literal => parse_literal(input),
         _ => parse_operator(input),
     }
 }
@@ -111,6 +139,7 @@ fn parse_packet_from_hex(input: &str) -> Packet {
 
 fn packet(input: &str) -> IResult<&str, Packet> {
     let (rest, (version, type_id)) = parse_header(&input)?;
+    let type_id = Type::from_u64(type_id);
     let (rest, payload) = payload(rest, type_id)?;
     Ok((
         rest,
@@ -132,6 +161,45 @@ impl Packet {
             match &self.payload {
                 Literal(_) => 0,
                 PacketPayload::SubPacket(packets) => packets.iter().map(|p| p.version_sum()).sum(),
+            }
+        }
+    }
+
+    fn value(&self) -> u64 {
+        match &self.payload {
+            Literal(val) => *val,
+            PacketPayload::SubPacket(subpackets) => {
+                let mut values = subpackets.iter().map(|p| p.value());
+                match &self.type_id {
+                    Type::Sum => values.sum(),
+                    Type::Product => values.product(),
+                    Type::Minimum => values.min().unwrap(),
+                    Type::Maximum => values.max().unwrap(),
+                    Type::Literal => {
+                        unreachable!()
+                    }
+                    Type::Greater => {
+                        if values.next() > values.next() {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    Type::Less => {
+                        if values.next() < values.next() {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    Type::Equal => {
+                        if values.next() == values.next() {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                }
             }
         }
     }
@@ -161,7 +229,7 @@ mod test {
         assert_eq!(
             Packet {
                 version: 6,
-                type_id: 4,
+                type_id: Type::from_u64(4),
                 payload: Literal(2021),
             },
             Packet::from_str("D2FE28")
@@ -173,16 +241,16 @@ mod test {
         assert_eq!(
             Packet {
                 version: 1,
-                type_id: 6,
+                type_id: Type::from_u64(6),
                 payload: SubPacket(vec![
                     Packet {
                         version: 6,
-                        type_id: 4,
+                        type_id: Type::from_u64(4),
                         payload: Literal(10),
                     },
                     Packet {
                         version: 2,
-                        type_id: 4,
+                        type_id: Type::from_u64(4),
                         payload: Literal(20),
                     },
                 ]),
@@ -196,21 +264,21 @@ mod test {
         assert_eq!(
             Packet {
                 version: 7,
-                type_id: 3,
+                type_id: Type::from_u64(3),
                 payload: SubPacket(vec![
                     Packet {
                         version: 2,
-                        type_id: 4,
+                        type_id: Type::from_u64(4),
                         payload: Literal(1),
                     },
                     Packet {
                         version: 4,
-                        type_id: 4,
+                        type_id: Type::from_u64(4),
                         payload: Literal(2),
                     },
                     Packet {
                         version: 1,
-                        type_id: 4,
+                        type_id: Type::from_u64(4),
                         payload: Literal(3),
                     },
                 ]),
@@ -225,16 +293,16 @@ mod test {
         assert_eq!(
             Packet {
                 version: 4,
-                type_id: 2,
+                type_id: Type::from_u64(2),
                 payload: PacketPayload::SubPacket(vec![Packet {
                     version: 1,
-                    type_id: 2,
+                    type_id: Type::from_u64(2),
                     payload: PacketPayload::SubPacket(vec![Packet {
                         version: 5,
-                        type_id: 2,
+                        type_id: Type::from_u64(2),
                         payload: PacketPayload::SubPacket(vec![Packet {
                             version: 6,
-                            type_id: 4,
+                            type_id: Type::from_u64(4),
                             payload: Literal(15),
                         }]),
                     }]),
@@ -270,11 +338,11 @@ mod test {
 
     #[test]
     fn part_one() {
-        assert_eq!(10, Packet::from_str("E20D41802B2984BD00540010F82D09E35880350D61A41D3004E5611E585F40159ED7AD7C90CF6BD6BE49C802DEB00525272CC1927752698693DA7C70029C0081002140096028C5400F6023C9C00D601ED88070070030005C2201448400E400F40400C400A50801E20004C1000809D14700B67676EE661137ADC64FF2BBAD745B3F2D69026335E92A0053533D78932A9DFE23AC7858C028920A973785338832CFA200F47C81D2BBBC7F9A9E1802FE00ACBA44F4D1E775DDC19C8054D93B7E72DBE7006AA200C41A8510980010D8731720CB80132918319804738AB3A8D3E773C4A4015A498E680292B1852E753E2B29D97F0DE6008CB3D4D031802D2853400D24DEAE0137AB8210051D24EB600844B95C56781B3004F002B99D8F635379EDE273AF26972D4A5610BA51004C12D1E25D802F32313239377B37100105343327E8031802B801AA00021D07231C2F10076184668693AC6600BCD83E8025231D752E5ADE311008A4EA092754596C6789727F069F99A4645008247D2579388DCF53558AE4B76B257200AAB80107947E94789FE76E36402868803F0D62743F00043A1646288800084C3F8971308032996A2BD8023292DF8BE467BB3790047F2572EF004A699E6164C013A007C62848DE91CC6DB459B6B40087E530AB31EE633BD23180393CBF36333038E011CBCE73C6FB098F4956112C98864EA1C2801D2D0F319802D60088002190620E479100622E4358952D84510074C0188CF0923410021F1CE1146E3006E3FC578EE600A4B6C4B002449C97E92449C97E92459796EB4FF874400A9A16100A26CEA6D0E5E5EC8841C9B8FE37109C99818023A00A4FD8BA531586BB8B1DC9AE080293B6972B7FA444285CC00AE492BC910C1697B5BDD8425409700562F471201186C0120004322B42489A200D4138A71AA796D00374978FE07B2314E99BFB6E909678A0").version_sum());
+        assert_eq!(984, Packet::from_str("E20D41802B2984BD00540010F82D09E35880350D61A41D3004E5611E585F40159ED7AD7C90CF6BD6BE49C802DEB00525272CC1927752698693DA7C70029C0081002140096028C5400F6023C9C00D601ED88070070030005C2201448400E400F40400C400A50801E20004C1000809D14700B67676EE661137ADC64FF2BBAD745B3F2D69026335E92A0053533D78932A9DFE23AC7858C028920A973785338832CFA200F47C81D2BBBC7F9A9E1802FE00ACBA44F4D1E775DDC19C8054D93B7E72DBE7006AA200C41A8510980010D8731720CB80132918319804738AB3A8D3E773C4A4015A498E680292B1852E753E2B29D97F0DE6008CB3D4D031802D2853400D24DEAE0137AB8210051D24EB600844B95C56781B3004F002B99D8F635379EDE273AF26972D4A5610BA51004C12D1E25D802F32313239377B37100105343327E8031802B801AA00021D07231C2F10076184668693AC6600BCD83E8025231D752E5ADE311008A4EA092754596C6789727F069F99A4645008247D2579388DCF53558AE4B76B257200AAB80107947E94789FE76E36402868803F0D62743F00043A1646288800084C3F8971308032996A2BD8023292DF8BE467BB3790047F2572EF004A699E6164C013A007C62848DE91CC6DB459B6B40087E530AB31EE633BD23180393CBF36333038E011CBCE73C6FB098F4956112C98864EA1C2801D2D0F319802D60088002190620E479100622E4358952D84510074C0188CF0923410021F1CE1146E3006E3FC578EE600A4B6C4B002449C97E92449C97E92459796EB4FF874400A9A16100A26CEA6D0E5E5EC8841C9B8FE37109C99818023A00A4FD8BA531586BB8B1DC9AE080293B6972B7FA444285CC00AE492BC910C1697B5BDD8425409700562F471201186C0120004322B42489A200D4138A71AA796D00374978FE07B2314E99BFB6E909678A0").version_sum());
     }
 
     #[test]
     fn part_two() {
-        assert!(false)
+        assert_eq!(984, Packet::from_str("E20D41802B2984BD00540010F82D09E35880350D61A41D3004E5611E585F40159ED7AD7C90CF6BD6BE49C802DEB00525272CC1927752698693DA7C70029C0081002140096028C5400F6023C9C00D601ED88070070030005C2201448400E400F40400C400A50801E20004C1000809D14700B67676EE661137ADC64FF2BBAD745B3F2D69026335E92A0053533D78932A9DFE23AC7858C028920A973785338832CFA200F47C81D2BBBC7F9A9E1802FE00ACBA44F4D1E775DDC19C8054D93B7E72DBE7006AA200C41A8510980010D8731720CB80132918319804738AB3A8D3E773C4A4015A498E680292B1852E753E2B29D97F0DE6008CB3D4D031802D2853400D24DEAE0137AB8210051D24EB600844B95C56781B3004F002B99D8F635379EDE273AF26972D4A5610BA51004C12D1E25D802F32313239377B37100105343327E8031802B801AA00021D07231C2F10076184668693AC6600BCD83E8025231D752E5ADE311008A4EA092754596C6789727F069F99A4645008247D2579388DCF53558AE4B76B257200AAB80107947E94789FE76E36402868803F0D62743F00043A1646288800084C3F8971308032996A2BD8023292DF8BE467BB3790047F2572EF004A699E6164C013A007C62848DE91CC6DB459B6B40087E530AB31EE633BD23180393CBF36333038E011CBCE73C6FB098F4956112C98864EA1C2801D2D0F319802D60088002190620E479100622E4358952D84510074C0188CF0923410021F1CE1146E3006E3FC578EE600A4B6C4B002449C97E92449C97E92459796EB4FF874400A9A16100A26CEA6D0E5E5EC8841C9B8FE37109C99818023A00A4FD8BA531586BB8B1DC9AE080293B6972B7FA444285CC00AE492BC910C1697B5BDD8425409700562F471201186C0120004322B42489A200D4138A71AA796D00374978FE07B2314E99BFB6E909678A0").value());
     }
 }
