44922083af914d4a7199b2f252feee332ffcdc66
Thomas <tduindam@gmail.com>
2021-12-26 13:58:58 +0100

    day 18 - never want to see this code again

--- src/day18.rs
+++ src/day18.rs
@@ -5,6 +5,7 @@ use std::fmt::Formatter;
 use std::rc::Rc;
 use std::sync::atomic::{AtomicI64, Ordering};
 
+use itertools::Itertools;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::sequence::{preceded, separated_pair, terminated};
@@ -106,79 +107,95 @@ enum Action {
     Split(RcNumber),
 }
 
-fn reduce(number: RcNumber) -> RcNumber {
-    let mut actions = VecDeque::<Action>::new();
-
-    let mut check_first = Vec::<usize>::new();
-    let mut only_explosions = false;
-    let mut only_splits = false;
-
-    'outer: loop {
-        let mut flat_map = flatten(number.clone(), 0, None);
-        println!(
-            "Reduction step {} {:?}",
-            number.clone().borrow(),
-            check_first
-        );
-        let extra_step = !check_first.is_empty();
-        let range: Box<dyn Iterator<Item = usize>> = if !extra_step {
-            only_explosions = false;
-            only_splits = false;
-
-            Box::new(0..flat_map.len())
+fn needs_explosion(n: &RcNumber) -> bool {
+    let parent = n.borrow().parent.as_ref().unwrap().clone();
+    if let PairNumber(left, right) = &parent.borrow().content {
+        let left_reg = if let Regular(_) = &left.borrow().content {
+            true
         } else {
-            Box::new(check_first.clone().into_iter())
+            false
         };
-        check_first.clear();
-        for i in range {
-            let must_explode = { flat_map[i].borrow().depth } > 4 && !only_splits;
-
-            let must_split = {
-                if let Regular(x) = flat_map[i].borrow().content {
-                    x >= 10
-                } else {
-                    false
-                }
-            } && !only_explosions;
-            if extra_step {
-                println!(
-                    "Checking [{}]{} must_explode {} must_split {}",
-                    i,
-                    flat_map[i].borrow(),
-                    must_explode,
-                    must_split
-                );
-            }
-            if must_explode {
-                println!(
-                    "Exploding: [{}] {} {}",
-                    i,
-                    flat_map[i].borrow(),
-                    flat_map[i + 1].borrow()
-                );
-                check_first = explode(&flat_map, i);
-                only_splits = true;
-                only_explosions = false;
-                if !extra_step {
-                    continue 'outer;
-                }
-            }
-            if must_split {
-                println!("Split: {}", flat_map[i].borrow());
-                check_first = split(&flat_map, i);
-                only_explosions = true;
-                only_splits = false;
-
-                if !extra_step {
-                    continue 'outer;
-                }
-            }
-        }
-        if extra_step {
-            continue 'outer;
-        }
-        return number;
+        let right_reg = if let Regular(_) = &right.borrow().content {
+            true
+        } else {
+            false
+        };
+        return left_reg && right_reg && left.borrow().depth > 4;
     }
+    false
+}
+
+fn needs_split(n: &RcNumber) -> bool {
+    if let Regular(x) = &n.borrow().content {
+        return *x >= 10;
+    }
+    false
+}
+
+fn find_next_action(root: RcNumber) -> Option<Action> {
+    let flattened = flatten(root.clone());
+    let explode = flattened
+        .iter()
+        .filter_map(|n| {
+            if needs_explosion(n) {
+                Some(Action::Explode(n.clone()))
+            } else {
+                None
+            }
+        })
+        .next();
+
+    let split = flattened
+        .iter()
+        .filter_map(|n| {
+            if needs_split(n) {
+                Some(Action::Split(n.clone()))
+            } else {
+                None
+            }
+        })
+        .next();
+    if let Some(explode) = explode {
+        Some(explode)
+    } else {
+        flattened
+            .iter()
+            .filter_map(|n| {
+                if needs_split(n) {
+                    Some(Action::Split(n.clone()))
+                } else {
+                    None
+                }
+            })
+            .next()
+    }
+}
+
+fn reduce(root: RcNumber) -> RcNumber {
+    let mut actions = VecDeque::<Action>::new();
+    if let Some(action) = find_next_action(root.clone()) {
+        actions.push_front(action);
+    }
+    while let Some(action) = actions.pop_front() {
+        let flat_map = flatten(root.clone());
+        match action {
+            Action::Explode(number) => {
+                explode(number.clone(), flat_map);
+            }
+            Action::Split(number) => {
+                let to_explode = split(number.clone());
+                if let Some(number) = to_explode {
+                    actions.push_front(Action::Explode(number));
+                }
+            }
+        }
+        if actions.is_empty() {
+            if let Some(action) = find_next_action(root.clone()) {
+                actions.push_front(action);
+            }
+        }
+    }
+    return root;
 }
 
 fn magnitude(number: RcNumber) -> i64 {
@@ -203,48 +220,69 @@ fn add(left: Option<RcNumber>, right: RcNumber) -> Option<RcNumber> {
         Some(left) => {
             let num = Number::new_rc(PairNumber(left, right));
             reduce(num.clone());
-            println!("Step: {}", num.borrow());
             Some(num)
         }
     }
 }
 
-fn split(flat_map: &Vec<RcNumber>, i: usize) -> Vec<usize> {
-    let value = extract_value(flat_map[i].clone()) as f64;
+fn split(number: RcNumber) -> Option<RcNumber> {
+    let value = extract_value(number.clone()) as f64;
     let left = Regular((value / 2f64).floor() as i64);
     let right = Regular((value / 2f64).ceil() as i64);
-    let new_pair = PairNumber(Number::new_rc(left), Number::new_rc(right));
-    flat_map[i].borrow_mut().content = new_pair;
-    vec![i]
+    let mut left = Number::new_rc(left);
+    let right = Number::new_rc(right);
+    let new_pair = PairNumber(left.clone(), right.clone());
+    left.borrow_mut().parent = Some(number.clone());
+    right.borrow_mut().parent = Some(number.clone());
+    number.borrow_mut().content = new_pair;
+
+    if needs_explosion(&left) {
+        Some(left.clone())
+    } else {
+        None
+    }
 }
 
-fn explode(flat_map: &Vec<RcNumber>, i: usize) -> Vec<usize> {
-    let parent = { flat_map[i].borrow().parent.as_ref().unwrap().clone() };
+fn explode(number: RcNumber, flat_map: Vec<RcNumber>) -> Vec<RcNumber> {
+    let i = flat_map
+        .iter()
+        .position(|n| n.borrow().id == number.borrow().id);
+
+    let i = i.unwrap();
+    let number = flat_map[i].clone();
+    let parent = { number.borrow().parent.as_ref().unwrap().clone() };
     let (l_val, r_val) = if let PairNumber(left, right) = &parent.borrow().content {
         (extract_value(left.clone()), extract_value(right.clone()))
     } else {
         unreachable!()
     };
-    let mut to_check = Vec::<usize>::new();
-    if i > 0 {
-        let cur = extract_value(flat_map[i - 1].clone());
-        flat_map[i - 1].borrow_mut().content = Regular(cur + l_val);
-        to_check.push(i - 1);
-    }
+    let mut split = Vec::<RcNumber>::new();
+
     //Add 2 since this function will trigger on left most of pair
     if i + 2 < flat_map.len() {
         let cur = extract_value(flat_map[i + 2].clone());
         flat_map[i + 2].borrow_mut().content = Regular(cur + r_val);
-        to_check.push(i + 1);
+        if needs_split(&flat_map[i + 2]) {
+            split.push(flat_map[i + 2].clone());
+        }
+    }
+    if i > 0 {
+        let cur = extract_value(flat_map[i - 1].clone());
+        flat_map[i - 1].borrow_mut().content = Regular(cur + l_val);
+        if needs_split(&flat_map[i - 1]) {
+            split.push(flat_map[i - 1].clone());
+        }
     }
     parent.borrow_mut().content = Regular(0);
-    to_check
+
+    split
 }
 
 fn extract_value(left: RcNumber) -> i64 {
     if let Regular(l) = &left.borrow().content {
         *l
     } else {
+        println!("{}", left.borrow());
         unreachable!()
     }
 }
@@ -294,6 +332,7 @@ fn parse_pair_primary(input: &str) -> RcNumber {
 
 #[cfg(test)]
 mod test {
+
     use super::*;
     use crate::reader::{read_lines_filter_ok, split_lines};
 
@@ -515,6 +554,17 @@ mod test {
 
     #[test]
     fn part_two() {
-        assert!(false);
+        let lines = read_lines_filter_ok("input/day18");
+        let combinations = lines.into_iter().combinations(2);
+        let max = combinations
+            .map(|mut two| {
+                let one = magnitude(Rc::new(RefCell::new(sum(two.clone()))));
+                two.reverse();
+                let two = magnitude(Rc::new(RefCell::new(sum(two))));
+                i64::max(one, two)
+            })
+            .max()
+            .unwrap();
+        assert_eq!(0, max);
     }
 }
